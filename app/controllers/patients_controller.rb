aef4a5c9a67d8f55ed26a66e87d1ca6bb7c796ae
Jamie Brown <jmbrowncow@gmail.com>
2018-08-27 18:29:43 +0000

    automatically backed up by learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,14 @@
 class PatientsController < ApplicationController
 
-  before_action :set_patient, only: [:show, :edit, :update]
+  before_action :set_patient, only: [:show]
+
+  def index
+    @doctors=Doctor.all
+  end
+
+  def show
+  end
+
 
   def set_patient
     @patient = Patient.find_by_id(params[:id])
