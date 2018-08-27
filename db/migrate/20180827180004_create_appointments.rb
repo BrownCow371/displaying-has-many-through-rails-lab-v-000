fd8959d42ebe811d077b746e78e8f5fa7771c18d
Jamie Brown <jmbrowncow@gmail.com>
2018-08-27 18:02:29 +0000

    automatically backed up by learn

--- db/migrate/20180827180004_create_appointments.rb
+++ db/migrate/20180827180004_create_appointments.rb
@@ -1,7 +1,7 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.string :name
+      t.string :appointment_datetime
 
       t.timestamps null: false
     end
