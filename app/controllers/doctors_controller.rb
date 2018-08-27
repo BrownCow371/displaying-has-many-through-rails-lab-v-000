738cbe1205f759912d54849f09747a288b3dbda2
Jamie Brown <jmbrowncow@gmail.com>
2018-08-27 18:29:16 +0000

    automatically backed up by learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -3,12 +3,13 @@ class DoctorsController < ApplicationController
   before_action :set_doctor, only: [:show, :edit, :update]
 
   def index
+    @doctors=Doctor.all
   end
 
   def show
   end
 
-  
+
   def set_doctor
     @doctor = Doctor.find_by_id(params[:id])
   end
